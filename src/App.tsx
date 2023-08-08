031815314e164bef59877df980508e72349345a1
Leonardo Mello Trindade <91926132+LeonardoMelloTrindade@users.noreply.github.com>
2023-08-07 21:58:29 -0300

    add font pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ModalInfo from "./components/modal/modalInfo";
 import "./App.css";
 import "./pokemon-font.css";
 import Store from "./data/Store.";
+import ToastErro from "./components/toast/toastErro";
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
           <section className="container_Pokemon">
             <ContainerPokemon />
           </section>
-          <section className="section_invisible"><ModalInfo/></section>
+          <section className="section_invisible"><ModalInfo/> </section>
+          <ToastErro/>
         </main>
     </Store>
   );
