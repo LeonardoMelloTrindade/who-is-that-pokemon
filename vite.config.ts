8b8dea092c00849c3bd8d84ef144582f7b41d1b1
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2026-01-26 23:41:59 -0300

    chore: fix problem route base to production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/who-is-that-pokemon"
+export default defineConfig(({ command: _command, mode: _mode }) => {
+  // Use /who-is-that-pokemon for GitHub Pages, / for Vercel and other deployments
+  const base = process.env.GITHUB_ACTIONS ? "/who-is-that-pokemon" : "/"
+
+  return {
+    plugins: [react()],
+    base
+  }
 })
